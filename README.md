e3e037e4cd5d205b3142a6216971f760ee9ffd96
Manuel Cabezas <manuelcabezas@gmail.com>
2024-09-19 04:03:40 -0300

    style: adjust message

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # QR Code Reader
 
-Este es un proyecto sencillo para leer códigos QR utilizando JavaScript (o cualquier otra tecnología que prefieras). Es un proyecto ideal para integrarlo en aplicaciones web, móviles o de escritorio.
+Este es un proyecto sencillo para leer códigos QR utilizando JavaScript. Ideal para integrarlo en aplicaciones web, móviles o de escritorio.
--- js/qr.js
+++ js/qr.js
@@ -8,12 +8,25 @@ function onScanSuccess(decodedText, decodedResult) {
         lastResult = decodedText;
         let exist = data[decodedText]
         if (exist){
-            $resultContainer.innerHTML  = `<p>&#128512 ¡VÁLIDO!</p>
-                                           <p>Patente: ${decodedText}</p>
-                                           <p>Congregación: ${exist.cng}</p>
-                                           <p>Acceso: ${exist.rol}</p>`
+            $resultContainer.innerHTML  = `<p>&#128512 <span class="badge text-bg-success">¡VÁLIDO!</span></p>
+                                            <table class="table">
+                                              <thead>
+                                                <tr>
+                                                <th scope="col">Patente</th>
+                                                <th scope="col">Congregación</th>
+                                                <th scope="col">Acceso</th>
+                                                </tr>
+                                              </thead>
+                                              <tbody>
+                                                <tr class="table-success">
+                                                <th scope="row">${decodedText}</th>
+                                                 <td>${exist.cng}</td>
+                                                 <td>${exist.rol}</td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
         } else {
-            $resultContainer.innerHTML  = '<p>&#128532 NO HABILITADO</p>'
+            $resultContainer.innerHTML  = '<p>&#128532 <span class="badge text-bg-danger">NO HABILITADO</span></p>'
         }
     }
 }
