56090221d6681938d5f9ae2cc103215fdf016efb
Manuel Cabezas <manuelcabezas@gmail.com>
2024-09-19 02:37:26 -0300

    fix: initialization c

--- data.js
+++ data.js
@@ -1,14 +1,4 @@
-export const data = {
-    YR9375: { cng: c[1], rol: r[1] },
-    JKGT75: { cng: c[6], rol: r[1] },
-    CC4689: { cng: c[7], rol: r[1] },
-    LKVZ39: { cng: c[2], rol: r[2]  }
-}
-
-const r ={
-  1: "Administración",
-  2: "Necesidades Especiales"
-}
+const r ={ 1: "Administración", 2: "Necesidades Especiales"}
 
 const c = {
   1:"Los Pinos",
@@ -18,4 +8,11 @@ const c = {
   5:"Panamericana",
   6:"Aguas Rojas",
   7:"Sagrada Familia"
+}
+
+export const data = {
+  YR9375: { cng: c[1], rol: r[1] },
+  JKGT75: { cng: c[6], rol: r[1] },
+  CC4689: { cng: c[7], rol: r[1] },
+  LKVZ39: { cng: c[2], rol: r[2]  }
 }
